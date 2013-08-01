cda95c996fe19d61c3693957903d3b023c2dde88
Victor Martinez <knoopx@gmail.com>
2013-08-01 17:47:11 +0200

    Optionally take a logger to log matches

--- lib/converge/middleware.rb
+++ lib/converge/middleware.rb
@@ -1,14 +1,16 @@
+require 'logger'
 require 'converge/engine'
 
 module Converge
   class Middleware
-    def initialize(app)
-      @app = app
+    def initialize(app, logger = Logger.new(nil))
+      @app, @logger = app, logger
     end
 
     def call(env)
       engine = ::Converge::Engine.new(env, ::Converge.rules)
       if engine.rewritten?
+        @logger.info "[Converge] Request redirected. Match: #{engine.match.args.inspect} at #{engine.match.block.source_location.join(":")}"
         [engine.code, engine.headers, engine.body]
       else
         @app.call(env)
